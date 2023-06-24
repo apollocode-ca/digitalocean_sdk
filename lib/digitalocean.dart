475939145c2c5220bd81ee490c8250199da63ef7
Étienne Bouchard <etiennebouchard7@gmail.com>
2023-06-24 14:53:50 -0400

    Fixed Small import errors

--- lib/digitalocean.dart
+++ lib/digitalocean.dart
@@ -1,8 +1,5 @@
 library digitalocean;
 
 export 'src/models/app_entity.dart';
-export 'src/models/apps/invoice_entity.dart';
-export 'src/models/apps/pricing/price_entity.dart';
-export 'src/models/apps/product_entity.dart';
 
 export 'src/digitalocean.dart' show DigitalOcean;
